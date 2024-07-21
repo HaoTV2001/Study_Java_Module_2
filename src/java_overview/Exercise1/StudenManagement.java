7f3fa013f3c8a36da2fe3bc05df871480e321d31
HaoTV2001 <Hao2001.dev@gmail.com>
2024-07-21 16:53:14 +0700

    Commit

---
+++ src/java_overview/Exercise1/StudenManagement.java
@@ -0,0 +1,10 @@
+package java_overview.Exercise1;
+
+public class StudenManagement {
+
+  public static void main(String[] args) {
+    Student student = new Student();
+    student.input();
+    student.output();
+  }
+}
