8a32fd5c38856e33e31167cde6cddb5148f829fd
HaoTV2001 <Hao2001.dev@gmail.com>
2024-07-21 17:39:11 +0700

    Commit

---
+++ src/java_overview/Exercise3/DistanceCalculator.java
@@ -0,0 +1,17 @@
+package java_overview.Exercise3;
+
+public class DistanceCalculator {
+
+  public static void main(String[] args) {
+    Point point = new Point();
+    Point anotherPoint = new Point();
+
+    System.out.print("Nhap toa do diem thu nhat: ");
+    point.input();
+
+    System.out.print("Nhap toa do diem thu hai: ");
+    anotherPoint.input();
+
+    System.out.print("Khoang cach giua hai diem la: "+point.distanceTo(anotherPoint));
+  }
+}
