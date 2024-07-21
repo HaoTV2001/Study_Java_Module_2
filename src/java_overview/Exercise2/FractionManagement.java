e0a9968f27e12e708ba7e4d322fd87c84e048c7c
HaoTV2001 <Hao2001.dev@gmail.com>
2024-07-21 17:31:34 +0700

    Commit

---
+++ src/java_overview/Exercise2/FractionManagement.java
@@ -0,0 +1,45 @@
+package java_overview.Exercise2;
+
+public class FractionManagement {
+
+  public static void main(String[] args) {
+    Fraction fraction_1 = new Fraction();
+    Fraction fraction_2 = new Fraction();
+
+    System.out.println("Nhap phan so thu nhat");
+    fraction_1.input();
+
+    System.out.println("Nhap phan so thu hai");
+    fraction_2.input();
+
+    System.out.print("Phan so thu nhat: ");
+    fraction_1.output();
+
+    System.out.print("Phan so thu hai: ");
+    fraction_2.output();
+
+    System.out.print("Tong hai phan so: ");
+    Fraction sum = fraction_1.sum(fraction_2);
+    sum.output();
+
+    System.out.print("Hieu hai phan so: ");
+    Fraction subtract = fraction_1.subtract(fraction_2);
+    subtract.output();
+
+    System.out.print("Tich hai phan so: ");
+    Fraction multiply = fraction_1.multiply(fraction_2);
+    multiply.output();
+
+    System.out.print("Thuong hai phan so: ");
+    Fraction divide = fraction_1.divide(fraction_2);
+    divide.output();
+
+    System.out.println("Phan so thu nhat la phan so " +(fraction_1.isPositiveFraction() ? "Duong" : "Am") + ".");
+
+    System.out.println("Phan so thu hai la phan so " +(fraction_2.isPositiveFraction() ? "Duong" : "Am") + ".");
+
+    System.out.println("Phan so thu nhat " + (fraction_1.isZeroFraction() ? "= 0" : "!= 0") + ".");
+
+    System.out.println("Phan so thu hai " + (fraction_2.isZeroFraction() ? "= 0" : "!= 0") + ".");
+  }
+}
